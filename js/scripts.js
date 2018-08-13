cb94bd4f2b02001deb4723d8733771acb1505e09
Colin Cook and Maly Phongsavanh <maly@>
2018-08-13 11:03:29 -0700

    adds everthing to the exercise

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $('#favorites').submit(function(event) {
+  event.preventDefault();
+  var que1 = $('input#question1').val();
+  var que2 = $('input#question2').val();
+  var que3 = $('input#question3').val();
+  var que4 = $('input#question4').val();
+  var que5 = $('input#question5').val();
+
+  var answers = [que1, que2, que3, que4, que5];
+
+  var three = [answers.push(answers[4])];
+
+  $('#answers').text(answers);
+  $('#2favorites').text(answers[0], " and ", answers[1]);
+  $('#3favorites').text(answers);
+
+  $("#que1").text(answers[0]);
+  $("#que2").text(answers[1]);
+  $("#que3").text(answers[2]);
+  $("#que4").text(answers[3]);
+  $("#que5").text(answers[4]);
+  $("#que6").text(answers[5]);
+
+  $('#output').show();
+
+  });
+});
